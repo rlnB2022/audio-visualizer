64bdcfa2cedb054f25532ba62269f398e6caee34
rlnB2022 <beyer.richard73@gmail.com>
2023-11-09 14:56:58 -0600

    Making changes for a fun new record visualizer look

--- js/index.js
+++ js/index.js
@@ -1,36 +1,22 @@
+// create the canvas and get the 2d context
 let canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
 
+// set the canvas height and width
 canvas.width = document.body.clientWidth;
-canvas.height = document.body.clientHeight;
+canvas.height = document.body.clientHeight - 15;
 
 let centerX = canvas.width / 2;
 let centerY = canvas.height / 2;
 let running = false;
-let rectY = [];
+let rectLY = [];
+let rectRY = [];
 
 // Create points
-for(let x = 0; x < 255; x++) {
-    rectY.push(0);
+for (let x = 0; x < 255; x++) {
+	rectLY.push(0);
+	rectRY.push(0);
 }
-// for(let angle = 0; angle < 360; angle += interval) {
-//   let distUp = 1.1;
-//   let distDown = 0.9;
-
-//   pointsUp.push({
-//     angle: angle + angleExtra,
-//     x: centerX + radius * Math.cos((-angle + angleExtra) * Math.PI / 180) * distUp,
-//     y: centerY + radius * Math.sin((-angle + angleExtra) * Math.PI / 180) * distUp,
-//     dist: distUp
-//   });
-
-//   pointsDown.push({
-//     angle: angle + angleExtra + 5,
-//     x: centerX + radius * Math.cos((-angle + angleExtra + 5) * Math.PI / 180) * distDown,
-//     y: centerY + radius * Math.sin((-angle + angleExtra + 5) * Math.PI / 180) * distDown,
-//     dist: distDown
-//   });
-// }
 
 const context = new AudioContext();
 const splitter = context.createChannelSplitter();
@@ -56,169 +42,194 @@ function loadAudio() {
 	audio.loop = false;
 	audio.autoplay = false;
 	audio.crossOrigin = "anonymous";
-	
-	audio.addEventListener('canplay', handleCanplay);
-	audio.src = "./eyeofthetiger.mp3";
+
+	audio.addEventListener("canplay", handleCanplay);
+	audio.src = "./ifeelforyou.mp3";
 	audio.load();
 	running = true;
 }
 
 function handleCanplay() {
-  // connect the audio element to the analyser node and the analyser node
-  // to the main Web Audio context
-  const source = context.createMediaElementSource(audio);
-  source.connect(splitter);
-  splitter.connect(context.destination);
+	// connect the audio element to the analyser node and the analyser node
+	// to the main Web Audio context
+	const source = context.createMediaElementSource(audio);
+	source.connect(splitter);
+	splitter.connect(context.destination);
 }
 
 function toggleAudio() {
-  if (running === false) {
-    loadAudio();
-    // document.querySelector('.call-to-action').remove();
-  }
+	if (running === false) {
+		loadAudio();
+	}
 
-  if (audio.paused) {
-    audio.play();
-  } else {
-    audio.pause();
-  }
+	if (audio.paused) {
+		audio.play();
+	} else {
+		audio.pause();
+	}
 }
 
-canvas.addEventListener('click', toggleAudio);
+canvas.addEventListener("click", toggleAudio);
 
-document.body.addEventListener('touchend', function(ev) {
-  context.resume();
+document.body.addEventListener("touchend", function (ev) {
+	context.resume();
 });
 
 function drawText(points) {
-    let letters = ['J','A','V','A','S','C','R','I','P','T'];
+	let letters = ["J", "A", "V", "A", "S", "C", "R", "I", "P", "T"];
 
-    for(let i=0; i < 10; i++) {
-        let ada = audioDataArrayL[i] / 255;
+	for (let i = 0; i < 10; i++) {
+		let ada = audioDataArrayL[i] / 255;
 
-        ctx.font = (points[0] * .200) + 'px serif';
-        ctx.fillText(letters[i], 400 + (i*75), 500);
-        // ctx.fillText('J', 400, 500);
-        // ctx.fillText('A', 450, 500);
-        // ctx.fillText('V', 500, 500);
-        // ctx.fillText('A', 550, 500);
-        // ctx.fillText('S', 600, 500);
-        // ctx.fillText('C', 650, 500);
-        // ctx.fillText('R', 700, 500);
-        // ctx.fillText('I', 750, 500);
-        // ctx.fillText('P', 800, 500);
-        // ctx.fillText('T', 850, 500);
-    }
+		ctx.font = points[0] * ada + "px serif";
+		ctx.fillText(letters[i], 400 + i * 125, centerY);
+	}
 }
 
-function drawSquare(points) {
-    for(let i=0; i < 255; i++) {
-        let ada = audioDataArrayL[i] / 255;
-        ctx.beginPath();
-        ctx.rect(centerX - (3 * i), centerY - points[i] -50, 1, 10 + points[i]);
-        if(ada >= .8) {
-            ctx.strokeStyle = "#ff0000";
-        }
-        else if(ada >= .7 && ada < .8) {
-            ctx.strokeStyle = "#ff038e";
-        }
-        else if(ada >= .6 && ada < .7) {
-            ctx.strokeStyle = "#ff05ff";
-        }
-        else if(ada >= .5 && ada < .6) {
-            ctx.strokeStyle = "#a805ff";
-        }
-        else if(ada >= .4 && ada < .5) {
-            ctx.strokeStyle = "#2605ff";
-        }
-        else if(ada >= .3 && ada < .4) {
-            ctx.strokeStyle = "#05a3ff";
-        }
-        else if(ada >= .2 && ada < .3) {
-            ctx.strokeStyle = "#05ff69";
-        }
-        else if(ada >= .1 && ada < .2) {
-            ctx.strokeStyle = "#ffee05";
-        }
-        else {
-            ctx.strokeStyle = "#000000";
-        }
-        ctx.closePath();
-        ctx.stroke();
-    }
+// draw each of the frequency data integers
+// these represent the decibel value for a specific frequency
+function drawSquare(points, audioSide) {
+	let modifier = 0;
+	const rightSide = audioSide === "R";
+
+	if (rightSide) {
+		modifier = 500;
+	}
+	for (let i = 0; i < 255; i++) {
+		let ada;
+		if (rightSide) {
+			ada = audioDataArrayR[i] / 255;
+		} else {
+			ada = audioDataArrayL[i] / 255;
+		}
+		ctx.beginPath();
+		ctx.rect(
+			centerX - 3 * i + modifier,
+			900 - points[i] - 50,
+			1,
+			0 + points[i]
+		);
+		if (ada >= 0.8) {
+			ctx.strokeStyle = "#ff0000";
+		} else if (ada >= 0.7 && ada < 0.8) {
+			ctx.strokeStyle = "#ff038e";
+		} else if (ada >= 0.6 && ada < 0.7) {
+			ctx.strokeStyle = "#ff05ff";
+		} else if (ada >= 0.5 && ada < 0.6) {
+			ctx.strokeStyle = "#a805ff";
+		} else if (ada >= 0.4 && ada < 0.5) {
+			ctx.strokeStyle = "#2605ff";
+		} else if (ada >= 0.3 && ada < 0.4) {
+			ctx.strokeStyle = "#05a3ff";
+		} else if (ada >= 0.2 && ada < 0.3) {
+			ctx.strokeStyle = "#05ff69";
+		} else if (ada >= 0.1 && ada < 0.2) {
+			ctx.strokeStyle = "#ffee05";
+		} else {
+			ctx.strokeStyle = "#000000";
+		}
+		ctx.closePath();
+		ctx.stroke();
+	}
 }
-
-// function drawLine(points) {
-//   let origin = points[0];
-  
-//   ctx.beginPath();
-//   ctx.strokeStyle = 'rgba(255,255,255,0.5)';
-//   ctx.lineJoin = 'round';
-//   ctx.moveTo(origin.x, origin.y);
-
-//   for (let i = 0; i < points.length; i++) {
-//     ctx.lineTo(points[i].x, points[i].y);
-//   }
-
-//   ctx.lineTo(origin.x, origin.y);
-//   ctx.stroke();
-// }
-
-// function connectPoints(pointsA, pointsB) {
-//   for (let i = 0; i < pointsA.length; i++) {
-//     ctx.beginPath();
-//     ctx.strokeStyle = 'rgba(255,255,255,0.5)';
-//     ctx.moveTo(pointsA[i].x, pointsA[i].y);
-//     ctx.lineTo(pointsB[i].x, pointsB[i].y);
-//     ctx.stroke();
-//   }
-// }
 
 function update(dt) {
-  let audioValueY;
+	let audioValueLY;
+	let audioValueRY;
 
-  // get the current audio data
-  analyserL.getByteFrequencyData(audioDataArrayL);
-  analyserR.getByteFrequencyData(audioDataArrayR);
+	// get the current audio data
+	analyserL.getByteFrequencyData(audioDataArrayL);
+	analyserR.getByteFrequencyData(audioDataArrayR);
 
-//   for (let i = 0; i < pointsUp.length; i++) {
-    // audioIndex = Math.ceil(pointsUp[i].angle * (bufferLengthL / (pCircle * 2))) | 0;
-        // get the audio data and make it go from 0 to 1
-    for(let i=0; i < 255; i++) {
-        audioValueY = audioDataArrayL[i] / 255;
-    
-        rectY[i] = audioValueY * 400;
-    }
-    // console.log(audioValue);
+	// get the audio data and make it go from 0 to 1
+	for (let i = 0; i < 255; i++) {
+		audioValueLY = audioDataArrayL[i] / 255;
+		audioValueRY = audioDataArrayL[i] / 255;
 
-    // pointsUp[i].dist = 1.1 + audioValue * .8;
-    // pointsUp[i].x = centerX + radius * Math.cos(-pointsUp[i].angle * Math.PI / 180) * pointsUp[i].dist;
-    // pointsUp[i].y = centerY + radius * Math.sin(-pointsUp[i].angle * Math.PI / 180) * pointsUp[i].dist;
-
-    // audioIndex = Math.ceil(pointsDown[i].angle * (bufferLengthR / (pCircle * 2))) | 0;
-    // get the audio data and make it go from 0 to 1
-    // audioValue = audioDataArrayR[audioIndex] / 255;
-
-    // pointsDown[i].dist = 0.9 + audioValue * 0.2;
-    // pointsDown[i].x = centerX + radius * Math.cos(-pointsDown[i].angle * Math.PI / 180) * pointsDown[i].dist;
-    // pointsDown[i].y = centerY + radius * Math.sin(-pointsDown[i].angle * Math.PI / 180) * pointsDown[i].dist;
-//   }
+		rectLY[i] = audioValueLY * canvas.height;
+		rectRY[i] = audioValueRY * canvas.height;
+	}
 }
 
 function draw(dt) {
-    requestAnimationFrame(draw);
+	requestAnimationFrame(draw);
 
-    if (running) {
-        update(dt);
-    }
+	if (running) {
+		update(dt);
+	}
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	/********* DRAWING VERTICAL LINES *********/
+	// drawSquare(rectLY, "L");
+	// drawSquare(rectRY, "R");
 
-//   drawLine(pointsUp);
-//   drawLine(pointsDown);
-//   connectPoints(pointsUp, pointsDown);
-    // drawSquare(rectY);
-    drawText(rectY);
+	/********* DRAWING TEXT *********/
+	// drawText(rectLY);
+	// drawText(rectRY);
+
+	/********* DRAWING SPINNING RECORD *******/
+	drawAlbumInfo();
+	drawSongInfo();
+	drawTimeUp();
+	drawRecord();
+	drawTimeLeft();
+}
+
+/**
+ * Red circlea round the record that shows how much time is left
+ * by reducing it's arc degrees
+ */
+function drawTimeLeft() {}
+
+function drawRecord() {
+	/* vinyl part of record */
+	ctx.fillStyle = "#000000";
+	ctx.beginPath();
+	ctx.arc(canvas.width - 500, centerY, 200, 0, 2 * Math.PI);
+	ctx.fill();
+
+	/* center of record */
+	ctx.fillStyle = "#ffffff";
+	ctx.beginPath();
+	ctx.arc(canvas.width - 500, centerY, 50, 0, 2 * Math.PI);
+	ctx.fill();
+}
+
+function drawTimeUp() {
+	// convert audio.currentTime into minutes and seconds
+	const minutes = Math.floor(audio.currentTime / 60);
+	const seconds = Math.floor(audio.currentTime - minutes * 60);
+	// Draw the amount of time listened
+	ctx.font = "18px serif";
+	ctx.fillStyle = "#cccccc";
+	ctx.fillText(
+		`${minutes}:${seconds < 10 ? "0" + seconds : seconds}`,
+		canvas.width - 400,
+		100
+	);
+}
+
+function drawAlbumInfo() {
+	// Album Header
+	ctx.font = "36px serif";
+	ctx.fillStyle = "#ffffff";
+	ctx.fillText("Album", 300, 100);
+
+	// Album Name
+	ctx.font = "18px serif";
+	ctx.fillStyle = "#ffffff";
+	ctx.fillText("Name", 300, 125);
+}
+
+function drawSongInfo() {
+	// Song Header
+	ctx.font = "48px serif";
+	ctx.fillStyle = "#ffffff";
+	ctx.fillText("Song", 300, canvas.height / 2 + 100);
+	// Song Name
+	ctx.font = "24px serif";
+	ctx.fillStyle = "#ffffff";
+	ctx.fillText("Name", 300, canvas.height / 2 + 125);
 }
 
 draw();
